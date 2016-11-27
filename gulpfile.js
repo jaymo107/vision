a117e6c1f171f3fcd58080b5bbadee010d7c113d
James Davies <jaymo107@gmail.com>
2016-11-27 17:40:10 +0000

    INitial

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minify = require('gulp-cssmin');
+var connect = require('gulp-connect');
+var autoprefixer = require('gulp-autoprefixer');
+var changed = require('gulp-changed');
+var babelify = require('babelify');
+var stringify = require('stringify');
+
+/**
+ * Path to the scripts to be used in the application,
+ * include each one in the order to be imported to
+ * satisfy dependancies.
+ */
+var scripts = [
+    './lib/js/app.js'
+];
+
+/**
+ * Build the Javascript files including assets into a single
+ * minified JS file.
+ */
+gulp.task('build', function() {
+    return browserify({
+        entries: scripts,
+        debug: true
+    }).transform(babelify)
+        .transform(stringify({
+            minify: true,
+            appliesTo: { includeExtensions: ['.html', '.tmpl', '.tpl'] }
+        }))
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./public/dist/js/'));
+});
+
+/**
+ * Compile SASS and concatenate into a single CSS file.
+ */
+gulp.task('styles', function() {
+    return gulp.src('./lib/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('app.css'))
+        .pipe(minify())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./public/dist/css/'));
+});
+
+/**
+ * Initialise the connection to the development server to
+ * allow for live reloading.
+ */
+gulp.task('connect', function() {
+    connect.server({
+        livereload: true
+    });
+});
+
+/**
+ * Watch and automatically re-run these tasks when the files
+ * change in the directories.
+ */
+gulp.task('watch', [], function() {
+    gulp.watch('./lib/js/**/*.*', ['build']);
+    gulp.watch('./lib/sass/**/*.scss', ['styles']);
+});
+
+/**
+ * Default gulp task, run them all.
+ */
+gulp.task('default', ['build', 'connect', 'watch']);
---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1" user-scalable=no”>
+	<title>Vision</title>
+	<link rel="stylesheet" type="text/css" href="dist/css/app.css">
+</head>
+<body>
+<div class="container"></div>
+<script src="https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js"></script>
+<script src="dist/js/app.js"></script>
+</body>
+</html>

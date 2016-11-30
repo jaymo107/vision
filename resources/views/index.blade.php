e77d51165135a7b5f4f420d542f7e2c74ca3553a
James Davies <jaymo107@gmail.com>
2016-11-30 12:03:15 +0000

    Pour integration

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,12 @@
 	<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1" user-scalable=no”>
 	<title>Vision</title>
 	<link rel="stylesheet" type="text/css" href="dist/css/app.css">
-    <link href="//vjs.zencdn.net/5.8/video-js.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.plyr.io/2.0.11/plyr.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
 <div class="container"></div>
+<script src="https://cdn.plyr.io/2.0.11/plyr.js"></script>
 <script src="https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js"></script>
 <script src="dist/js/app.js"></script>
 </body>

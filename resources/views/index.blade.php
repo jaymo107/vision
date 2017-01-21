4f48f11e906b8c6b55d091e2eb289fadcfbc850f
James Davies <jaymo107@gmail.com>
2017-01-21 20:25:53 +0000

    Full width

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1" user-scalable=no”>
-	<title>Vision</title>
-	<link rel="stylesheet" type="text/css" href="dist/css/app.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1" user-scalable=no”>
+    <title>Vision</title>
+    <link rel="stylesheet" type="text/css" href="dist/css/app.css">
     <link rel="stylesheet" href="https://cdn.plyr.io/2.0.11/plyr.css">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
-<div class="container"></div>
+<div class="container full-width"></div>
 <script type="text/javascript">
 
-	window.App = {
-	    user: {
-	        id: '{{ $userId }}'
-	    },
+    window.App = {
+        user: {
+            id: '{{ $userId }}'
+        },
 
-	    api: '{{ $apiKey }}'
-	};
-
+        api: '{{ $apiKey }}'
+    };
 
 </script>
 <script src="https://cdn.plyr.io/2.0.11/plyr.js"></script>
